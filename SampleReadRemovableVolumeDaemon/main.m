be6cc87f6f5d253dfa756f7243bea5bdbd3759cd
Danil Korotenko <danil.korotenko@gmail.com>
2025-05-05 16:53:46 +0300

    Initial Commit

---
+++ SampleReadRemovableVolumeDaemon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleReadRemovableVolumeDaemon
+//
+//  Created by Danil Korotenko on 5/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
